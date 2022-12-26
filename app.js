7ccb0adc7cc58036eed3e3db68919fdae82288b3
Alona <alona.costa2@gmail.com>
2022-12-26 15:20:54 +0000

    Request

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// HTTP (Hypertext Transfer protocol)
-// Request - What do we want to do
-// Response - What was actually done
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 const game1 = new Hangman('Cat Love', 4)
@@ -16,20 +13,18 @@ console.log(game1.status)
     guessesEl.textContent = game1.statusMessage  
   })
 
-// Making an HTTP request
-const request = new XMLHttpRequest()
-
-request.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4 && e.target.status === 200) {    
-    const data = JSON.parse(e.target.responseText)
-    console.log(data)
-  } else if (e.target.readyState === 4) {
-    console.log('And error has taken place')
-  } 
+getPuzzle((error, puzzle) => {
+  if (error) {
+    console.log(`Error: ${error}`)
+  } else {
+    console.log(puzzle)
+  }
+  
 })
+  
 
-request.open('GET', 'https://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
+// Making an HTTP request
+/*
 
 const countryCode = 'UA'
 const contryRequest = new XMLHttpRequest()
@@ -45,5 +40,5 @@ contryRequest.addEventListener('readystatechange', (e) => {
 })
 
 contryRequest.open('GET', 'https://restcountries.com/v3.1/all')
-contryRequest.send()
+contryRequest.send()*/
 
