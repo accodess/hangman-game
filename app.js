ee94a4ae391863f69f84862f61e98067d8ea89f7
Alona <alona.costa2@gmail.com>
2022-12-27 11:17:57 +0000

    Callback

--- app.js
+++ app.js
@@ -13,32 +13,21 @@ console.log(game1.status)
     guessesEl.textContent = game1.statusMessage  
   })
 
-getPuzzle((error, puzzle) => {
+getPuzzle('2', (error, puzzle) => {
   if (error) {
     console.log(`Error: ${error}`)
   } else {
     console.log(puzzle)
-  }
-  
+  }  
 })
-  
 
-// Making an HTTP request
-/*
-
-const countryCode = 'UA'
-const contryRequest = new XMLHttpRequest()
-
-contryRequest.addEventListener('readystatechange', (e) => {
-  if (e.target.readyState === 4 && e.target.status === 200) {
-    const data = JSON.parse(e.target.responseText)
-    const country = data.find((country) => country.cca2 === countryCode)
-    console.log(country.name.common)    
-  } else if (e.target.readyState === 4) {
-    console.log('Unable to fetch data')
+getCountry('MX', (error, country) => {
+  if (error) {
+    console.log(`Error: ${error}`)
+  } else {
+    console.log(`Country name: ${country}`)
   }
 })
 
-contryRequest.open('GET', 'https://restcountries.com/v3.1/all')
-contryRequest.send()*/
+
 
--- requests.js
+++ requests.js
@@ -1,4 +1,4 @@
-const getPuzzle = (callback) => {
+const getPuzzle = (wordCount, callback) => {
 
   const request = new XMLHttpRequest()
 
@@ -11,6 +11,24 @@ const getPuzzle = (callback) => {
     } 
   })
   
-  request.open('GET', 'https://puzzle.mead.io/puzzle?wordCount=3')
+  request.open('GET', `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
   request.send()
 }
+
+const getCountry = (countryCode, callback) => {
+  const contryRequest = new XMLHttpRequest()
+  
+  contryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+      const data = JSON.parse(e.target.responseText)
+      const country = data.find((country) => country.cca2 === countryCode)
+      callback(undefined, country.name.common)
+    } else if (e.target.readyState === 4) {
+      callback('Unable to fetch data', undefined)     
+    }
+  })
+  
+  contryRequest.open('GET', 'https://restcountries.com/v3.1/all')
+  contryRequest.send()
+}
+
